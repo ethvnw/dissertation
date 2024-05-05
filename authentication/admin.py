905783880eb419ae25ffac985129e1704f11837a
Ethan Watts <ethan.watts@live.co.uk>
2024-05-05 13:29:18 +0100

    add users to admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .forms import StudentSignUpForm
 from .models import User, Student
 
-admin.site.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'department', 'role', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name', 'department', 'role')
+
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'first_name', 'last_name', 'department', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'department', 'role'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Student)
